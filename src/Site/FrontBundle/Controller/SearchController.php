e5fe1dda04f7587a68af059bac2de3580d758b77
aprusevic <prusevic@gmail.com>
2013-08-18 11:25:15 +0200

    frontend templates

--- src/Site/FrontBundle/Controller/SearchController.php
+++ src/Site/FrontBundle/Controller/SearchController.php
@@ -13,12 +13,20 @@ class SearchController extends Controller
     public function resultsAction( )
     {
         $q = $this->get('request')->query->get('q',null);
+        $p = $this->get('request')->query->get('page',1);
         $em = $this->getDoctrine()->getManager();
-       $entities = $em->getRepository('ArticleBundle:Post')->searchPostsBy($q);
+        $entities = $em->getRepository('ArticleBundle:Post')->searchPostsBy($q);
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+        $entities,
+        $p,
+        5/*limit per page*/
+        );
+      
         return $this->render('FrontBundle:Search:results.html.twig',
             array(
                 'results' => $q,
-                'posts' => $entities
+                'posts' => $pagination
                 
             ) );
 
