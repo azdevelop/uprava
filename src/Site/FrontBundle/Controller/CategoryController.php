6f83a0b2eec5d74bea016bc849fd8fc030d5d5f4
aprusevic <prusevic@gmail.com>
2013-08-19 22:39:24 +0200

    category pages, post slug

---
+++ src/Site/FrontBundle/Controller/CategoryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Site\FrontBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class CategoryController extends Controller
+{
+
+
+    public function indexAction( $category, $locale )
+    {
+        $p = $this->get('request')->query->get('page',1);
+        $em = $this->getDoctrine()->getManager();
+        $category = $em->getRepository('CategoryBundle:Category')->findOneBy(array('name' => $category));
+        $category->setTranslatableLocale($locale);
+        $em->refresh($category);
+        $posts = $category->getPosts();
+    
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $posts,
+            $p,
+            5/*limit per page*/
+        );
+        return $this->render('FrontBundle:Category:index.html.twig',
+            array(
+                'posts' => $pagination,
+                'category' => $category
+            )
+        );
+    }
+    
+     
+
+}
